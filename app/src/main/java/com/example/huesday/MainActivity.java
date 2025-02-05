cf959c6be489204eb9e5e08cc331c964b7fbf7e0
Seham Othman <210709718@posta.mu.edu.tr>
2025-02-06 01:39:37 +0300

    main page done

--- app/src/main/java/com/example/huesday/MainActivity.java
+++ app/src/main/java/com/example/huesday/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.huesday;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    ToggleButton todayButton;
+    ToggleButton pixelsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        todayButton = findViewById(R.id.todayButton);
+        pixelsButton = findViewById(R.id.piexlsButton);
+
+        //today fragment will be opened!
+        todayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //pixels fragment will be opened!
+        pixelsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
         });
     }
+
 }
